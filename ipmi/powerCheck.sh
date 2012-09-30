8321d7d338096a67a4a6a2a6d5d27dde6df7db46
James Rudd <james.rudd@gmail.com>
2012-10-01 00:07:00 +1000

    ipmi

--- ipmi/powerCheck.sh
+++ ipmi/powerCheck.sh
@@ -3,7 +3,13 @@ passfile=~ruddj/ipmipass
 
 for i in {0..13} 
 do
- echo "Dell-0-$i"
- ipmitool -I lan -H manager-0-$i -f $passfile -U root chassis power status  
+ echo "Compute-1-$i"
+ ipmitool -I lan -H ipmi-1-$i.ipmi -f $passfile -U root chassis power status  
+done
+
+for i in {0..6}
+do
+ echo "Compute-2-$i"
+ ipmitool -I lan -H ipmi-2-$i.ipmi -f $passfile -U root chassis power status
 done
 
