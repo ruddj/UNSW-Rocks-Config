6e28e04ff169bd05af0235c01bba660d6999951b
James Rudd <James.Rudd@gmail.com>
2013-11-07 07:25:27 +1100

    IPMI

---
+++ ipmi/emailNotify.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+passfile=~ruddj/ipmipass
+dellPass=`cat $passfile`
+
+for i in {0..15}
+do
+ echo "Compute-2-$i"
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-2-$i.admin -u root -p $dellPass config -g cfgIpmiLan -o cfgIpmiLanAlertEnable 1
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-2-$i.admin -u root -p $dellPass config -g cfgEmailAlert -o cfgEmailAlertEnable -i 1 1
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-2-$i.admin -u root -p $dellPass config -g cfgEmailAlert -o cfgEmailAlertAddress -i 1 root@FyrHead.ad.unsw.edu.au
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-2-$i.admin -u root -p $dellPass config -g cfgRemoteHosts -o cfgRhostsSmtpServerIpAddr 10.1.1.1
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-2-$i.admin -u root -p $dellPass config -g cfgLanNetworking -o cfgDNSDomainName ad.unsw.edu.au
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-2-$i.admin -u root -p $dellPass config -g cfgLanNetworking -o cfgDNSRacName  Fyr-C2-$i
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-2-$i.admin -u root -p $dellPass config -g cfgLanNetworking -o cfgDNSServer1 10.1.1.1
+done
+
+for i in {0..5}
+do
+ echo "Compute-1-$i"
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-1-$i.admin -u root -p $dellPass config -g cfgIpmiLan -o cfgIpmiLanAlertEnable 1
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-1-$i.admin -u root -p $dellPass config -g cfgEmailAlert -o cfgEmailAlertEnable -i 1 1
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-1-$i.admin -u root -p $dellPass config -g cfgEmailAlert -o cfgEmailAlertAddress -i 1 root@FyrHead.ad.unsw.edu.au
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-1-$i.admin -u root -p $dellPass config -g cfgRemoteHosts -o cfgRhostsSmtpServerIpAddr 10.1.1.1
+done
+
+for i in {0..9}
+do
+ echo "Compute-0-$i"
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-0-$i.admin -u root -p $dellPass config -g cfgIpmiLan -o cfgIpmiLanAlertEnable 1
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-0-$i.admin -u root -p $dellPass config -g cfgEmailAlert -o cfgEmailAlertEnable -i 1 1
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-0-$i.admin -u root -p $dellPass config -g cfgEmailAlert -o cfgEmailAlertAddress -i 1 root@FyrHead.ad.unsw.edu.au
+ /opt/dell/srvadmin/sbin/racadm -r ipmi-0-$i.admin -u root -p $dellPass config -g cfgRemoteHosts -o cfgRhostsSmtpServerIpAddr 10.1.1.1
+done
+
