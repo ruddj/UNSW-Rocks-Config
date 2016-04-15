fc938f5013d710d3e34fb56c9e89232b4640e7b4
James Rudd <James.Rudd@gmail.com>
2016-04-15 13:59:34 +1000

    Spawn new process

--- ipmi/power-action.sh
+++ ipmi/power-action.sh
@@ -9,19 +9,19 @@ fi
 for i in {0..9} 
 do
  echo "Compute-0-$i"
- ipmitool -I lan -H ipmi-0-$i.admin -f $passfile -U root chassis power $1 
+ ipmitool -I lan -H ipmi-0-$i.admin -f $passfile -U root chassis power $1 &
 done
 
 for i in {0..5}
 do
  echo "Compute-1-$i"
- ipmitool -I lan -H ipmi-1-$i.admin -f $passfile -U root chassis power $1
+ ipmitool -I lan -H ipmi-1-$i.admin -f $passfile -U root chassis power $1 &
 done
 
 for i in {0..15}
 do
  echo "Compute-2-$i"
- ipmitool -I lanplus -H ipmi-2-$i.admin -f $passfile -U root -C1 chassis power $1
+ ipmitool -I lanplus -H ipmi-2-$i.admin -f $passfile -U root -C1 chassis power $1 &
 done
 
 
